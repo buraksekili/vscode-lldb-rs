fff3abcf235d0696d48ec27595e162c3aee8c838
Burak Sekili <32663655+buraksekili@users.noreply.github.com>
2025-01-30 17:00:06 +0300

    change cwd to root

--- lldb-server.sh
+++ lldb-server.sh
@@ -1,5 +1,8 @@
+#!/bin/sh
+
 echo "==> the lldb server will listen on lo:3999"
 echo "==> the lldb server's working directory is /"
 
+cd "/"
 sudo -E ~/.vscode/extensions/vadimcn.vscode-lldb-1.11.2/lldb/bin/lldb-server platform --server --listen "*:3999"
 
