55d7dacc14b8249f182119763d99cd96885892cd
Burak Sekili <32663655+buraksekili@users.noreply.github.com>
2025-01-30 16:51:47 +0300

    init commit

---
+++ lldb-server.sh
@@ -0,0 +1,5 @@
+echo "==> the lldb server will listen on lo:3999"
+echo "==> the lldb server's working directory is /"
+
+sudo -E ~/.vscode/extensions/vadimcn.vscode-lldb-1.11.2/lldb/bin/lldb-server platform --server --listen "*:3999"
+
